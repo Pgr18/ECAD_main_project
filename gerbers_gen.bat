b93254abb63895c6e42992af16c72484c1388d6c
Pgr18 <121094432+Pgr18@users.noreply.github.com>
2025-10-27 20:28:52 +0000

    Deploying to main from @ Pgr18/ECAD_main_project@fa77bc7d525d1915cc1f8815ceef3057f2a52317 🚀

--- gerbers_gen.bat
+++
@@ -1,2 +0,0 @@
-kicad-cli pcb export gerbers D:\ECAD_main_project\ECAD_main\ECAD_main.kicad_pcb --layers  F.Cu --output  D:\ECAD_main_project\
-kicad-cli pcb export gerbers D:\ECAD_main_project\ECAD_main\ECAD_main.kicad_pcb --layers  B.Cu --output  D:\ECAD_main_project\
