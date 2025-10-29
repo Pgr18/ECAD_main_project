0bd5740a0356d582e653711ab24dde6715d7b351
Pgr18 <paulry@mail.ru>
2025-10-29 19:47:30 +0300

    wip

---
+++ gerbers_gen.bat
@@ -0,0 +1,2 @@
+kicad-cli pcb export gerbers D:\ECAD_main_project\ECAD_main\ECAD_main.kicad_pcb --layers  F.Cu --output  D:\ECAD_main_project\
+kicad-cli pcb export gerbers D:\ECAD_main_project\ECAD_main\ECAD_main.kicad_pcb --layers  B.Cu --output  D:\ECAD_main_project\
